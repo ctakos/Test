d86fec000dad1ceb48dc1d8c5f1f98918a711464
Χρήστος Τάκος <takxristos1989@hotmail.com>
2018-12-11 20:18:57 +0200

    Commit

--- Test/Test/Form1.cs
+++ Test/Test/Form1.cs
@@ -12,9 +12,36 @@ namespace Test
 {
     public partial class Form1 : Form
     {
+        List<Panel> listPanel = new List<Panel>();
+        int index;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if(index > 0)
+            {
+                listPanel[--index].BringToFront();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index < listPanel.Count - 1)
+            {
+                listPanel[++index].BringToFront();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listPanel.Add(panel1);
+            listPanel.Add(panel2);
+            listPanel.Add(panel3);
+            listPanel[index].BringToFront();
         }
     }
 }
